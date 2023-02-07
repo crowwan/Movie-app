5182ee0a1d78841de2269f330eee793d01bde0e7
crowwan <crowwan@naver.com>
2023-02-07 20:37:56 +0900

    feat: 프로젝트 세팅

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+import Header from "./layouts/Header";
+
 function App() {
-  return <div className="App">app</div>;
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
 }
 
 export default App;
