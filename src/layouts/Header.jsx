eb649719c34a80e001e0ab3584f81a4f9dec6e45
crowwan <crowwan@naver.com>
2023-02-07 20:38:40 +0900

    feat: 컴포넌트 틀 완성
    컴포넌트 틀 만들기

---
+++ src/layouts/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Search from "../components/Search";
+
+function Header() {
+  return (
+    <header>
+      <Search />
+    </header>
+  );
+}
+
+export default Header;
