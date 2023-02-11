c9c2f7a5e0ff1fa24b51c22e2b30957e43a18b1e
crowwan <crowwan@naver.com>
2023-02-11 21:47:31 +0900

    feat: 영화 카드 컴포넌트 style components 적용 완료

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,7 +1,45 @@
 import React from "react";
-
+import styled from "styled-components";
+const StyledCard = styled.div``;
+const StyledBackground = styled.div`
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  width: 15rem;
+  height: 25rem;
+  margin-bottom: 2rem;
+  cursor: pointer;
+  & > img {
+    height: 80%;
+    object-fit: contain;
+  }
+`;
+const StyledTitle = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 1rem;
+  font-size: 0.9rem;
+  font-weight: bold;
+  background-color: #35447c;
+  color: #f3f3f3;
+`;
 function Card() {
-  return <div>Card</div>;
+  return (
+    <StyledCard>
+      <StyledBackground>
+        <img
+          src="https://image.tmdb.org/t/p/w1280/kuf6dutpsT0vSVehic3EZIqkOBt.jpg"
+          alt=""
+        />
+        <StyledTitle>
+          <span>Puss in Boots: The Last Wish</span>
+          <span>8.6</span>
+        </StyledTitle>
+      </StyledBackground>
+    </StyledCard>
+  );
 }
 
 export default Card;
