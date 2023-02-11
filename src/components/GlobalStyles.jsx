21c82e062e1794a5c344f1e0a56ec96271275b3c
crowwan <crowwan@naver.com>
2023-02-11 21:47:02 +0900

    feat: Global style component 생성 완료

---
+++ src/components/GlobalStyles.jsx
@@ -0,0 +1,72 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyles = createGlobalStyle`
+body, html {
+  color: #35447C;
+  background-color: #FBF4E4;
+  padding: 0;
+  margin: 0;
+}
+
+div,
+form,
+fieldset,
+section,
+main,
+table,
+td,
+tr,
+th,
+tbody,
+thead,
+button,
+input,
+label,
+span,
+header,
+ul,
+select,
+option,
+li,
+img,
+p,
+h1,
+h2,
+h3,
+h4,
+a,
+aside,
+nav,
+main,
+textarea,
+pre {
+  box-sizing: border-box;
+  padding: 0;
+  margin: 0;
+}
+
+a {
+  text-decoration: none;
+  color: inherit;
+}
+a:visited {
+  color: inherit;
+}
+li {
+  list-style-type: none;
+}
+th {
+  font-weight: normal;
+}
+table {
+  border-collapse: collapse;
+}
+button:hover {
+  cursor: pointer;
+}
+textarea {
+  resize: none;
+}
+`;
+
+export default GlobalStyles;
