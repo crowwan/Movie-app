b81600539cb171f127e9ea9a97342f5a243edcb9
crowwan <crowwan@naver.com>
2023-02-11 21:44:50 +0900

    feat: Global styled 컴포넌트 적용

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 import Header from "./layouts/Header";
+import Main from "./layouts/Main";
+import GlobalStyles from "./components/GlobalStyles";
+import styled from "styled-components";
 
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Container = styled.div`
+  width: 70vw;
+`;
 function App() {
   return (
-    <div className="App">
-      <Header />
-    </div>
+    <Wrapper>
+      <Container>
+        <GlobalStyles />
+        <div className="App">
+          <Header />
+          <Main />
+        </div>
+      </Container>
+    </Wrapper>
   );
 }
 
