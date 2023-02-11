8a9c8ab1baa0fb79f6f9cd5fcfea38b3354e2422
crowwan <crowwan@naver.com>
2023-02-11 21:45:32 +0900

    feat: styled 컴포넌트 적용 완료

--- src/layouts/Header.jsx
+++ src/layouts/Header.jsx
@@ -1,11 +1,19 @@
 import React from "react";
+import styled from "styled-components";
 import Search from "../components/Search";
+
+const HeaderStyled = styled.header`
+  padding: 2rem;
+  display: flex;
+  color: #35447c;
+  justify-content: space-between;
+`;
 
 function Header() {
   return (
-    <header>
+    <HeaderStyled>
       <Search />
-    </header>
+    </HeaderStyled>
   );
 }
 
