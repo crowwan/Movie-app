58bfe923186a20047c53605effb27d60956b8026
crowwan <crowwan@naver.com>
2023-02-11 21:46:30 +0900

    feat: input 컴포넌트 styled components 적용 완료

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,4 +1,23 @@
 import React, { useState } from "react";
+import styled from "styled-components";
+
+const Input = styled.input`
+  width: 70%;
+  outline: none;
+  font-size: 24px;
+  background-color: transparent;
+  border: none;
+  border-radius: 15px;
+  padding: 0.5rem 2rem;
+  box-shadow: 0 0 5px;
+`;
+const FilterBox = styled.select`
+  background-color: transparent;
+  border-radius: 15px;
+  border: none;
+  box-shadow: 0 0 5px;
+  width: 20%;
+`;
 
 function Search() {
   const [value, setValue] = useState("");
@@ -6,7 +25,12 @@ function Search() {
   const onChange = (e) => {
     setValue(e.target.value);
   };
-  return <input type="text" value={value} onChange={onChange} />;
+  return (
+    <>
+      <Input type="text" value={value} onChange={onChange} />
+      <FilterBox />
+    </>
+  );
 }
 
 export default Search;
