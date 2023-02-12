90657a51da911a09e11df6310bb14a03f6305076
crowwan <crowwan@naver.com>
2023-02-12 20:35:12 +0900

    feat: props 추가 및 동적 UI 작업

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,42 +1,44 @@
 import React from "react";
 import styled from "styled-components";
-const StyledCard = styled.div``;
+const StyledCard = styled.div`
+  width: 15rem;
+  height: 25rem;
+  margin-bottom: 2rem;
+`;
 const StyledBackground = styled.div`
   position: relative;
   display: flex;
   flex-direction: column;
   justify-content: space-between;
-  width: 15rem;
-  height: 25rem;
-  margin-bottom: 2rem;
+  height: 100%;
+  border-radius: 15px;
+  overflow: hidden;
   cursor: pointer;
-  & > img {
-    height: 80%;
-    object-fit: contain;
-  }
 `;
-const StyledTitle = styled.div`
+const StyledImage = styled.img`
+  height: 80%;
+  object-fit: cover;
+`;
+const StyledTitleBox = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: center;
+  height: 20%;
   padding: 1rem;
   font-size: 0.9rem;
   font-weight: bold;
   background-color: #35447c;
   color: #f3f3f3;
 `;
-function Card() {
+function Card({ imgUrl, data }) {
   return (
     <StyledCard>
       <StyledBackground>
-        <img
-          src="https://image.tmdb.org/t/p/w1280/kuf6dutpsT0vSVehic3EZIqkOBt.jpg"
-          alt=""
-        />
-        <StyledTitle>
-          <span>Puss in Boots: The Last Wish</span>
-          <span>8.6</span>
-        </StyledTitle>
+        <StyledImage src={`${imgUrl}/${data.poster_path}`} alt="" />
+        <StyledTitleBox>
+          <span>{data.title}</span>
+          <span>{data.vote_average}</span>
+        </StyledTitleBox>
       </StyledBackground>
     </StyledCard>
   );
