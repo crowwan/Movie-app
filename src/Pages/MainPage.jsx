27caf08be0aca872f24cc291ce47a1c2423e593f
crowwan <crowwan@naver.com>
2023-02-13 22:37:59 +0900

    feat: 메인 페이지 컴포넌트 생성

---
+++ src/Pages/MainPage.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import StyledContainer from "../components/StyledContainer";
+import Header from "../layouts/Header";
+import Main from "../layouts/Main";
+
+function MainPage() {
+  return (
+    <StyledContainer>
+      <Header />
+      <Main />
+    </StyledContainer>
+  );
+}
+
+export default MainPage;
