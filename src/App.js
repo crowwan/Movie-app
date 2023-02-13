46b3ea97720a5acc7aff226d70e4adb89e720b9a
crowwan <crowwan@naver.com>
2023-02-13 22:37:27 +0900

    feat: 라우터 처리 변경
    Main -> MainPage

--- src/App.js
+++ src/App.js
@@ -1,33 +1,14 @@
-import Header from "./layouts/Header";
-import Main from "./Pages/Main";
+import MainPage from "./Pages/MainPage";
 import GlobalStyles from "./components/GlobalStyles";
-import styled from "styled-components";
 import { Route, Routes } from "react-router-dom";
 import MoviePage from "./Pages/MoviePage";
 
-const Wrapper = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
-
-const Container = styled.div`
-  width: 70vw;
-`;
 function App() {
   return (
     <>
-      <Wrapper>
-        <Container>
-          <GlobalStyles />
-          <div className="App">
-            <Header />
-            <Main />
-          </div>
-        </Container>
-      </Wrapper>
+      <GlobalStyles />
       <Routes>
-        <Route path="/" element={<Main />} />
+        <Route path="/" element={<MainPage />} />
         <Route path="/movie/:id" element={<MoviePage />} />
       </Routes>
     </>
