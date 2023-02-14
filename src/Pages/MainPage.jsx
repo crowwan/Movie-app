44561dc3c02305cec76c6aab1f7b9e21dc1c2e22
crowwan <crowwan@naver.com>
2023-02-14 19:22:05 +0900

    feat: api url 이동

--- src/Pages/MainPage.jsx
+++ src/Pages/MainPage.jsx
@@ -1,13 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import StyledContainer from "../components/StyledContainer";
 import Header from "../layouts/Header";
 import Main from "../layouts/Main";
+import axios from "axios";
+import useSWR from "swr";
+import { API_URL_PATH } from "../data/urls";
+
+export const fetcher = (url) => axios.get(url).then((res) => res.data);
 
 function MainPage() {
+  const [page, setPage] = useState(1);
+  const { data, isLoading, error } = useSWR(
+    `${API_URL_PATH}?api_key=46779acb390ace5e9843e8f8a752dace&query=''`,
+    fetcher,
+    { shouldRetryOnError: false }
+  );
   return (
     <StyledContainer>
       <Header />
-      <Main />
+      <Main data={data} isLoading={isLoading} error={error} />
     </StyledContainer>
   );
 }
