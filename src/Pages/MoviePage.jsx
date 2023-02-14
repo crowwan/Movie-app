2585cbb3dc041d32d55db29460535e4cd1295b76
crowwan <crowwan@naver.com>
2023-02-14 19:22:55 +0900

    feat: swr을 이용한 데이터 패칭 구현
    Detail 컴포넌트에 필요한 데이터, Recommendations 컴포넌트에 필요한 데이터 패칭

--- src/Pages/MoviePage.jsx
+++ src/Pages/MoviePage.jsx
@@ -1,7 +1,31 @@
 import React from "react";
 import StyledContainer from "../components/StyledContainer";
+import Detail from "../layouts/Detail";
+import useSWR from "swr";
+import { fetcher } from "./MainPage";
+import { useParams } from "react-router-dom";
+import Recommendations from "../layouts/Recommendations";
+
 function MoviePage() {
-  return <StyledContainer></StyledContainer>;
+  const { id } = useParams();
+  const {
+    data: detail,
+    isLoading,
+    error,
+  } = useSWR(
+    `https://api.themoviedb.org/3/movie/${id}?api_key=46779acb390ace5e9843e8f8a752dace`,
+    fetcher
+  );
+  const { data: recommend } = useSWR(
+    `https://api.themoviedb.org/3/movie/${id}/recommendations?api_key=46779acb390ace5e9843e8f8a752dace`,
+    fetcher
+  );
+  return (
+    <StyledContainer>
+      <Detail data={detail} isLoading={isLoading} error={error} />
+      <Recommendations data={recommend} />
+    </StyledContainer>
+  );
 }
 
 export default MoviePage;
