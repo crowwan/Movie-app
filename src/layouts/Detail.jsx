f67376ff6df4ee5b5009436e934a3edb1c85cbc7
crowwan <crowwan@naver.com>
2023-02-15 17:25:17 +0900

    feat: 메인 페이지로 돌아가기 버튼 추가

--- src/layouts/Detail.jsx
+++ src/layouts/Detail.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { IMG_URL } from "../data/urls";
 import styled from "styled-components";
+import { useNavigate } from "react-router-dom";
+
 const StyledContainer = styled.div`
   display: flex;
   margin: 5rem 0;
@@ -20,35 +22,61 @@ const StyledTitle = styled.h2`
   border: 1px solid white;
   margin-bottom: 2rem;
 `;
+const StyledToMain = styled.div`
+  display: flex;
+  position: absolute;
+  padding: 0.5rem;
+  align-items: center;
+  height: 5rem;
+`;
+const StyledBtn = styled.span`
+  color: white;
+  font-size: 1rem;
+  cursor: pointer;
+`;
+const tags = ["status", "popularity", "runtime", "vote_average"];
 function Detail({ data, isLoading, error }) {
-  const tags = ["status", "popularity", "runtime", "vote_average"];
+  // TODO: overview 디자인
+  // TODO: 예매 버튼은 나중에
+  const naviagtion = useNavigate();
   return (
-    <StyledContainer>
-      {data && (
-        <>
-          <img src={`${IMG_URL}/${data.poster_path}`} />
-          <StyledSection>
-            <StyledTitle>{data.title}</StyledTitle>
-            <div>
+    <>
+      <StyledToMain>
+        <StyledBtn
+          onClick={() => {
+            naviagtion("/");
+          }}
+        >
+          {"<"} 메인으로
+        </StyledBtn>
+      </StyledToMain>
+      <StyledContainer>
+        {data && (
+          <>
+            <img src={`${IMG_URL}/${data.poster_path}`} />
+            <StyledSection>
+              <StyledTitle>{data.title}</StyledTitle>
               <div>
-                <h2>Overview</h2>
-                <p>{data.overview}</p>
+                <div>
+                  <h2>Overview</h2>
+                  <p>{data.overview}</p>
+                </div>
+                <div>
+                  <ul>
+                    {tags.map((a) => (
+                      <li key={a}>
+                        <span>{a}</span>
+                        <div>{data[a]}</div>
+                      </li>
+                    ))}
+                  </ul>
+                </div>
               </div>
-              <div>
-                <ul>
-                  {tags.map((a) => (
-                    <li>
-                      <span>{a}</span>
-                      <div>{data[a]}</div>
-                    </li>
-                  ))}
-                </ul>
-              </div>
-            </div>
-          </StyledSection>
-        </>
-      )}
-    </StyledContainer>
+            </StyledSection>
+          </>
+        )}
+      </StyledContainer>
+    </>
   );
 }
 
