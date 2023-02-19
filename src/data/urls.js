879655adaacda6068494b7add39cf99c00fb2ea5
crowwan <crowwan@naver.com>
2023-02-19 19:26:47 +0900

    feat: search api url 추가

--- src/data/urls.js
+++ src/data/urls.js
@@ -1,3 +1,5 @@
 const IMG_URL = "https://image.tmdb.org/t/p/w1280";
 const API_URL_PATH = "https://api.themoviedb.org/3/discover/movie";
-export { IMG_URL, API_URL_PATH };
+const SEARCH_API =
+  "https://api.themoviedb.org/3/search/movie?api_key=46779acb390ace5e9843e8f8a752dace";
+export { IMG_URL, API_URL_PATH, SEARCH_API };
