70f87e933879a53c44232e096fd598b8ae7e371e
crowwan <crowwan@naver.com>
2023-02-20 23:28:18 +0900

    feat: 더보기 버튼 추가, 페이지 관련 props 추가

--- src/layouts/Main.jsx
+++ src/layouts/Main.jsx
@@ -8,16 +8,35 @@ const StyledCardContainer = styled.div`
   flex-wrap: wrap;
   justify-content: space-between;
 `;
+const StyledButtonContainer = styled(StyledCardContainer)`
+  justify-content: center;
+  margin-bottom: 2rem;
+`;
+const StyledMoreBtn = styled.button`
+  background-color: #35447c;
+  border-radius: 10px;
+  padding: 1rem 2rem;
+  color: white;
+`;
 
-function Main({ data }) {
+function Main({ data, setPage, page, total_pages }) {
   console.log(data);
   return (
-    <StyledCardContainer>
-      {data &&
-        data.results.map((a) => (
-          <Card key={a.id} imgUrl={IMG_URL + a.poster_path} data={a} />
-        ))}
-    </StyledCardContainer>
+    <>
+      <StyledCardContainer>
+        {data &&
+          data.map((a) => (
+            <Card key={a.id} imgUrl={IMG_URL + a.poster_path} data={a} />
+          ))}
+      </StyledCardContainer>
+      {page < total_pages && (
+        <StyledButtonContainer>
+          <StyledMoreBtn onClick={() => setPage((prev) => prev + 1)}>
+            More
+          </StyledMoreBtn>
+        </StyledButtonContainer>
+      )}
+    </>
   );
 }
 
